10d57f4f7f10363036a635cb4aae0d2cc1fb44dc
Bilhalv <pedrokabilhalva@gmail.com>
2023-05-03 22:38:53 -0300

    Colapsing blocks

--- script.js
+++ script.js
@@ -10,3 +10,14 @@
     false
   );
 })();
+
+const toggleBtns = document.querySelectorAll('.toggle-btn');
+
+toggleBtns.forEach((btn) => {
+  const targetId = btn.dataset.target;
+  const target = document.getElementById(targetId);
+
+  btn.addEventListener('click', () => {
+    target.classList.toggle('show');
+  });
+});
