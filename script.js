e0d6be6042a3890b2f0d256f2868df1e9a7ca584
Vinicius Moraes <vmoraes424@gmail.com>
2023-06-08 17:44:15 -0300

    Org/Refatoração de código e menu dropdown arrumado

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-(function () {
-  // Bind Click event to the drop down navigation button
-  document.querySelector(".nav-button").addEventListener(
-    "click",
-    function () {
-      /*  Toggle the CSS closed class which reduces the height of the UL thus 
-          hiding all LI apart from the first */
-      this.parentNode.parentNode.classList.toggle("closed");
-    },
-    false
-  );
-})();
+const btn = document.querySelector(".drop-down-btn");
+const menu = document.querySelector(".drop-down");
 
-const toggleBtns = document.querySelectorAll('.toggle-btn');
-
-toggleBtns.forEach((btn) => {
-  const targetId = btn.dataset.target;
-  const target = document.getElementById(targetId);
-
-  btn.addEventListener('click', () => {
-    target.classList.toggle('show');
-  });
+btn.addEventListener("click", () => {
+  menu.classList.toggle("closed");
+  btn.classList.toggle("aberto");
 });
