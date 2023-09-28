b8bcd1be57076ddfd1917ead327b5eb6eaaaf20a
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2023-09-28 11:24:10 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: 'https://www.asstbmpel.com.br/', // Set the base path to your custom domain
+  base: '/', // Set the base path to your custom domain
   plugins: [react()],
   build: {
     outDir: 'build', // Make sure this matches your actual build directory
