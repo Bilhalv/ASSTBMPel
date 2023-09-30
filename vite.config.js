3dc1fccab297f362ce9695fe668d3dc43b041050
Pedro Bilhalva Oliveira <pedrokabilhalva@gmail.com>
2023-09-30 01:18:53 -0300

    Hotfix attempt

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-=======
-  base: '/', // Set the base path to your custom domain
->>>>>>> b8bcd1be57076ddfd1917ead327b5eb6eaaaf20a
   plugins: [react()],
 })
