e046803723a66d59e825fdb0ddc04b35c430bd38
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2023-10-20 17:41:36 +0000

    organização do fotter

--- src/components/Fotter.jsx
+++ src/components/Fotter.jsx
@@ -1,13 +1,38 @@
-import { AtSign, Facebook, Instagram, Phone, Smartphone } from 'lucide-react'
+import { AtSign, Facebook, Instagram, Phone, Smartphone } from "lucide-react";
 
 export default function Fotter() {
   return (
     <fotter className="bg-principal flex justify-between items-center py-6 px-5 shadow-md bottom-0">
-      <p className='flex'><AtSign/>asstbmpel@gmail.com</p>
-      <p className='flex'><Facebook/><a className='hover:underline hover:cursor-pointer' href='https://www.facebook.com/ASSTBMPEL'>ASSTBMPel</a></p>
-      <p className='flex'><Smartphone/>+55 (53) 9 9117-7141</p>
-      <p className='flex'><Phone/>+55 (53) 3228-4355</p>
-      <p className='flex'><Instagram/><a className='hover:underline hover:cursor-pointer' href='https://instagram.com/asstbmpel'>@asstbmpel</a></p>
+      <p className="flex">
+        <AtSign />
+        asstbmpel@gmail.com
+      </p>
+      <p className="flex">
+        <Facebook />
+        <a
+          className="hover:underline hover:cursor-pointer"
+          href="https://www.facebook.com/ASSTBMPEL"
+        >
+          ASSTBMPel
+        </a>
+      </p>
+      <p className="flex">
+        <Smartphone />
+        +55 (53) 9 9117-7141
+      </p>
+      <p className="flex">
+        <Phone />
+        +55 (53) 3228-4355
+      </p>
+      <p className="flex">
+        <Instagram />
+        <a
+          className="hover:underline hover:cursor-pointer"
+          href="https://instagram.com/asstbmpel"
+        >
+          @asstbmpel
+        </a>
+      </p>
     </fotter>
   );
 }
