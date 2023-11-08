976d9038cc952ca4abe640d7f46738b8ce34d65c
Pedro Bilhalva Oliveira <pedrokabilhalva@gmail.com>
2023-11-08 15:36:57 -0300

    attempt to fix the refresh bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "./pages/App.jsx";
 import Alugueis from "./pages/Alugueis.jsx";
 import Convenios from "./pages/Convenios.jsx";
@@ -11,43 +11,18 @@ import Locais from "./pages/Locais.jsx";
 import NaoEncontrado from "./pages/404.jsx";
 import '../dist/output.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/alugueis",
-    element: <Alugueis />,
-  },
-  {
-    path: "/convenios",
-    element: <Convenios />,
-  },
-  {
-    path: "/equipe",
-    element: <Equipe />,
-  },
-  {
-    path: "/esportes",
-    element: <Esporte />,
-  },
-  {
-    path: "/historico",
-    element: <Historico />,
-  },
-  {
-    path: "/locais",
-    element: <Locais />,
-  },
-  {
-    path: "/*",
-    element: <NaoEncontrado />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/alugueis" component={Alugueis} />
+      <Route path="/convenios" component={Convenios} />
+      <Route path="/equipe" component={Equipe} />
+      <Route path="/esporte" component={Esporte} />
+      <Route path="/historico" component={Historico} />
+      <Route path="/locais" component={Locais} />
+      <Route component={NaoEncontrado} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
 );
